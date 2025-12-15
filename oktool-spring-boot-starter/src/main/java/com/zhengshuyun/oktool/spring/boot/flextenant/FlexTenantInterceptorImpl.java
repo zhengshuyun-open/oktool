a13cbc8ad7a17394145a9dd9df066a72403edfbf
Toint <599818663@qq.com>
2025-12-16 02:46:02 +0800

    update groupId

--- oktool-spring-boot-starter/src/main/java/com/zhengshuyun/oktool/spring/boot/flextenant/FlexTenantInterceptorImpl.java
+++ oktool-spring-boot-starter/src/main/java/com/zhengshuyun/oktool/spring/boot/flextenant/FlexTenantInterceptorImpl.java
@@ -20,9 +20,6 @@ import cn.dev33.satoken.stp.StpUtil;
 import com.zhengshuyun.oktool.spring.boot.context.OkContext;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.web.servlet.HandlerInterceptor;
 
 import java.util.List;
 
